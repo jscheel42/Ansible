6278bf627099de321e489a77c8cc675cfcddcb24
Joshua Scheel <jscheel42@gmail.com>
2016-01-26 22:42:41 -0800

    Adding Debian 8 (Jessie) support to a boatload of roles

--- deploy-development.sh
+++ deploy-development.sh
@@ -3,8 +3,8 @@
 # CMD="ansible-playbook -i development site.yml --sudo --ask-sudo-pass"
 CMD="ansible-playbook -i development site.yml --sudo --ask-vault-pass"
 
-CMD="$CMD -l pod.homenet"
-# CMD="$CMD -t ssh-keys"
+CMD="$CMD -l dev-apps01.homenet"
+# CMD="$CMD -t postgresql"
 CMD="$CMD -v"
 
 # CMD="$CMD -e update_all_packages=yes"
