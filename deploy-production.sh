9f9ad10cc50bf66812dd98d0b8f8f1251336889e
Joshua Scheel <jscheel42@gmail.com>
2016-02-02 20:39:25 -0800

    add app_allc role fix some roles when run on debian

--- deploy-production.sh
+++ deploy-production.sh
@@ -3,11 +3,11 @@
 # CMD="ansible-playbook -i development site.yml --sudo --ask-sudo-pass"
 CMD="ansible-playbook -i production site.yml --sudo --ask-vault-pass"
 
-# CMD="$CMD -l linode-apps01"
-# CMD="$CMD -t app_rewards"
+CMD="$CMD -l dosfo-apps01.cloudnet"
+CMD="$CMD -t nginx"
 CMD="$CMD -v"
 
-CMD="$CMD -e update_all_packages=yes"
+# CMD="$CMD -e update_all_packages=yes"
 
 # $CMD -t firewall
 $CMD
