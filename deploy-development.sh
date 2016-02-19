ebd22edf202a3f0e216ca5e397c4c69563d18337
Joshua Scheel <jscheel42@gmail.com>
2016-02-18 17:01:31 -0800

    install steam package in opensuse

--- deploy-development.sh
+++ deploy-development.sh
@@ -3,11 +3,11 @@
 # CMD="ansible-playbook -i development site.yml --sudo --ask-sudo-pass"
 CMD="ansible-playbook -i development site.yml --sudo --ask-vault-pass"
 
-CMD="$CMD -l serenity.homenet"
+CMD="$CMD -l htpc.homenet"
 # CMD="$CMD -t hosts"
 CMD="$CMD -v"
 
-# CMD="$CMD -e update_all_packages=yes"
+CMD="$CMD -e update_all_packages=yes"
 
 # $CMD -t firewall
 $CMD
