74313069a1c674ad6a311a4cc1e1da6fbd8fbc90
Joshua Scheel <jscheel42@gmail.com>
2016-02-25 19:51:00 -0800

    update roles to support centos 7 on workstation

--- deploy-development.sh
+++ deploy-development.sh
@@ -3,11 +3,11 @@
 # CMD="ansible-playbook -i development site.yml --sudo --ask-sudo-pass"
 CMD="ansible-playbook -i development site.yml --sudo --ask-vault-pass"
 
-CMD="$CMD -l serenity.homenet,pod.homenet"
-CMD="$CMD -t firewall"
+CMD="$CMD -l pod.homenet"
+CMD="$CMD -t postgresql"
 # CMD="$CMD -v"
 
-CMD="$CMD -e update_all_packages=yes"
+#CMD="$CMD -e update_all_packages=yes"
 
 # $CMD -t firewall
 $CMD
