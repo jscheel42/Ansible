ee51a36c3354fb18cd4cb1cde1dce959b92b97fb
Joshua Scheel <jscheel42@gmail.com>
2016-10-16 13:29:46 -0700

    change bash prompt to multiline

--- deploy-development.sh
+++ deploy-development.sh
@@ -5,9 +5,11 @@
 
 # Laptop (pod.homenet) w/ extra vars to trick Ansible into thinking it is Ubuntu instead of Neon
 # Neon works w/ PPAs but the Ansible module breaks if it doesn't think you are using Ubuntu
-CMD="ansible-playbook -i development --extra-vars "@neon_shim.json" site.yml --sudo --ask-vault-pass"
+# CMD="ansible-playbook -i development --extra-vars "@neon_shim.json" site.yml --sudo --ask-vault-pass"
+
+CMD="ansible-playbook -i development site.yml --sudo --ask-vault-pass"
 CMD="$CMD -l pod.homenet"
-# CMD="$CMD -t pgadmin4"
+CMD="$CMD -t bash"
 # CMD="$CMD -v"
 
 #CMD="$CMD -e update_all_packages=yes"
